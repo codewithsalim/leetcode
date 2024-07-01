58119793346e0769992a9197d3003f5a0e0c2401
Salim Shaikh <saleems756@gmail.com>
2024-07-01 15:03:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (57.07%) - LeetSync

---
+++ Java/1293-three-consecutive-odds/three-consecutive-odds.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int oddCount = 0;
+        for ( int num : arr ){
+            if ( num%2 == 0 ){
+                oddCount = 0;
+            }else{
+                oddCount++;
+                if ( oddCount == 3 ) return true;
+            }
+        }
+        return false;
+    }
+}
